1096eaa0f57b725df29a9f3dd5711bf4acbfd323
Federico Pfund <federicopfund@gmail.com>
2022-05-14 19:56:05 -0300

    leccion 1 tipos de datos

---
+++ java/training/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,24 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello Java");
+       //Tipos de enteros y STRING
+       int miVariable = 10;
+       System.out.println(miVariable);
+       miVariable = 5;
+       System.out.println(miVariable);
+       // Tipos de String
+       String miVariableCadena = "Bienvenidos";
+       System.out.println(miVariableCadena);
+       miVariableCadena = "Sigamos creciendo en Programacion";
+       System.out.println(miVariableCadena);
+    }
+    
+}
