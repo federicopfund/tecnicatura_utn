ee18c7b5bf7146a2b94eb570f17a8c227715f5a2
Federico Pfund <federicopfund@gmail.com>
2022-06-12 18:43:09 -0300

    ejercicio 1  libros java

---
+++ java/training/java_utn/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+// importamos librerias
+import java.util.Scanner;
+/**
+ * @author federico pfund
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite el nombre del libro: ");
+        String nombreLibro = entrada.nextLine();
+        System.out.println("Digite el id del libro: ");
+        int idLibro = Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite el precio del libro: ");
+        double precioLibro = Double.parseDouble(entrada.nextLine());
+        System.out.println("Confirme si el envio es gratuito: ");
+        boolean envioGratuito = Boolean.parseBoolean(entrada.nextLine());
+        
+        // Mostramos por pantalla los resultados
+        System.out.println(nombreLibro+" #"+idLibro);
+        System.out.println("El precio es: $"+precioLibro);
+        System.out.println("El envio es gratuito?: "+envioGratuito);
+    }
+    
+}
+
+    
