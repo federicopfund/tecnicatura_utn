a2cdaf981354568c4efa11095a2c0d40da5a8c5c
Federico Pfund <74926730+federicopfund@users.noreply.github.com>
2022-06-20 10:47:41 -0300

    Tipos de datos java

--- java/training/java_utn/Ejercicio10/src/ejercicio10/Ejercicio10.java
+++ java/training/java_utn/Ejercicio10/src/ejercicio10/Ejercicio10.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio10;
 
 import java.util.Scanner;
 
 /**
  *
- * @author feder
+ * @author federico pfund
  */
 public class Ejercicio10 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite un mes del año: ");
