90a09fe4cb2ac640a6665dcb819b38ceea06a57e
federico pfund <federicopfund@gmail.com>
2022-09-23 23:56:19 -0300

    pasajes de argumentos, Operador "This" , metodos con argumentos

--- java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/Aritmetica.java
+++ java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/Aritmetica.java
@@ -9,7 +9,7 @@ public class Aritmetica {
     int a;
     int b;
     
-    // metodo void
+    // Metodo Void
     // type de retorno: vacio "void"
     public void sumarNumeros(){
         int resultado = a +b ;
@@ -23,9 +23,18 @@ public class Aritmetica {
    }
    // tipo de retorno type: entero
     public int sumarConArgumentos(int arg1,int arg2){
+        // El argumento a se asigna al atributo this.a 
+        // Una vez que se sale de la clase el operado se elimina
+        // Es opcional se hace referencia a un atributo y no a una variable local.
+        // Es como el self en python pero mas flexible
+        //this.a = arg1;
+        //this.b = arg2;
+        
         a = arg1;
         b = arg2;
-        return a + b;    
+         return a + b; 
+        
+        // Operados This apunta al espacio de memoria
+        //return this.sumarConArgumentos(3,2);
     }
-
 }
--- java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/PruevaAritmetica.java
+++ java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/PruevaAritmetica.java
@@ -4,17 +4,19 @@ package Operaciones;
 
 public class PruevaAritmetica {
     public static void main(String[] args) {
+        
+        // Definicion de resultado de forma global
         int resultado;
         // Nuevo Objeto llamando a un constructor
         Aritmetica aritmetica1 = new Aritmetica();
-        // ingresamos valores al objeto a traves de invocar el 
-        // obj por su correspondiente atributo
+        // Ingresamos valores al objeto a traves de invocar el 
+        // Obj por su correspondiente atributo
         aritmetica1.a = 3;
         aritmetica1.b = 7;
-        // invocamos al metodo sumar numero:
+        // Invocamos al metodo sumar numero:
         aritmetica1.sumarNumeros();
-        // crear una variable, type:int 
-        // invocar el metodo sumarConRetorno
+        // Crear una variable, type:int 
+        // Invocar el metodo sumarConRetorno
         int resutado = aritmetica1.sumarConRetorno();
         System.out.println("resutado = " + resutado);
         
