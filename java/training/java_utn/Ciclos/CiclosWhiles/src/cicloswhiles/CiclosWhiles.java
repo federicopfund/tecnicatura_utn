1fe5172d81588700db349c5d118e4848660c24e7
federico pfund <federicopfund@gmail.com>
2022-09-24 15:41:19 -0300

    ciclos while

---
+++ java/training/java_utn/Ciclos/CiclosWhiles/src/cicloswhiles/CiclosWhiles.java
@@ -0,0 +1,25 @@
+
+package cicloswhiles;
+
+
+public class CiclosWhiles {
+
+    
+    public static void main(String[] args) {
+        var conteo= 0;
+        while(conteo < 3){
+            System.out.println("conteo : " + conteo);
+            // Incrmentamos variable
+            conteo ++;
+        }
+       
+        var contador = 0;
+        do{
+            // block do
+            System.out.println("contador" + contador);
+            contador++;
+            
+        }while(contador < 7);
+    }
+    
+}
