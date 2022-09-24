366d1d56dfb12b0628ab705dd0b7187176c9dea6
federico pfund <federicopfund@gmail.com>
2022-09-23 23:34:45 -0300

    metodo con paso de argumento

--- java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/Aritmetica.java
+++ java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/Aritmetica.java
@@ -9,9 +9,23 @@ public class Aritmetica {
     int a;
     int b;
     
-    // metodo
-   public void sumarNumeros(){
+    // metodo void
+    // type de retorno: vacio "void"
+    public void sumarNumeros(){
         int resultado = a +b ;
          System.out.println("resultado = " + resultado);
-    } 
+    }
+   
+    // tipo de retorno type: entero
+    public int sumarConRetorno(){
+       // retornara una exprecion
+       return a + b;
+   }
+   // tipo de retorno type: entero
+    public int sumarConArgumentos(int arg1,int arg2){
+        a = arg1;
+        b = arg2;
+        return a + b;    
+    }
+
 }
--- java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/PruevaAritmetica.java
+++ java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/PruevaAritmetica.java
@@ -4,6 +4,7 @@ package Operaciones;
 
 public class PruevaAritmetica {
     public static void main(String[] args) {
+        int resultado;
         // Nuevo Objeto llamando a un constructor
         Aritmetica aritmetica1 = new Aritmetica();
         // ingresamos valores al objeto a traves de invocar el 
@@ -12,7 +13,13 @@ public class PruevaAritmetica {
         aritmetica1.b = 7;
         // invocamos al metodo sumar numero:
         aritmetica1.sumarNumeros();
+        // crear una variable, type:int 
+        // invocar el metodo sumarConRetorno
+        int resutado = aritmetica1.sumarConRetorno();
+        System.out.println("resutado = " + resutado);
         
         
+        resultado = aritmetica1.sumarConArgumentos(2, 3);
+        System.out.println("resultado usando arg: " + resultado);
     }    
 }
