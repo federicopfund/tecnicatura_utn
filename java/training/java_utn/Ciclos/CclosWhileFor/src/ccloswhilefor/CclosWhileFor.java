93c19c8e7cff302f8ab8343e74959b569f25fc63
federico pfund <federicopfund@gmail.com>
2022-09-24 16:53:41 -0300

    `Continue`  `Break`

--- java/training/java_utn/Ciclos/CclosWhileFor/src/ccloswhilefor/CclosWhileFor.java
+++ java/training/java_utn/Ciclos/CclosWhileFor/src/ccloswhilefor/CclosWhileFor.java
@@ -1,32 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ccloswhilefor;
 
-/**
- *
- * @author feder
- */
 public class CclosWhileFor {
-
-  
     public static void main(String[] args) {
+        // Definicion de variables
         var conteo= 0;
-       
+        var contador = 0;
+        // Mientras conteo < 3 ejecuta el block
         while(conteo < 3){
             System.out.println("conteo : " + conteo);
             // Incrmentamos variable
             conteo ++;
         }
-        var contador = 0;
-        do{ // block do
-            System.out.println("contador" + contador);
+        // Luego de ejecutar el block While entra en el block do
+        do{ System.out.println("contador" + contador);
             contador++;
         }while(contador < 7);
+        // Break = "ROMPE UNA ESTRUCTURA CONDICIONAL"
         // declarar var/condicion a cumplir/ incrememto o decremento del operador
         for(var contando = 0; contando < 7 ; contando++){
+            // Mostrara Numeros pares por una estructura condicional
+            if(contador % 2 == 0){
+                System.out.println("contando = " + contando);
+                break;
+            }
+        }
+        // Continue = "CONTINUE CON EL FLUJO DE EJECUCION". 
+        for(var contando = 0; contando < 7 ; contando++){
+            // Mostrara Numeros pares por una estructura condicional
+            if(contador % 2 == 0){
+                // Vamos a la Siguiente Iteracion.
+                continue;
+            }
             System.out.println("contando = " + contando);
         }
     }
