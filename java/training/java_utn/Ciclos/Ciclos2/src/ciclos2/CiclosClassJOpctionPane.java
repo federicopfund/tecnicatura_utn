3e5063fea2f0cf753f566964a2b014c58957a857
federico pfund <federicopfund@gmail.com>
2022-09-24 20:10:36 -0300

    con Clase `JOptionPanel`

--- java/training/java_utn/Ciclos/Ciclos2/src/ciclos2/CiclosClassJOpctionPane.java
+++ java/training/java_utn/Ciclos/Ciclos2/src/ciclos2/CiclosClassJOpctionPane.java
@@ -4,17 +4,13 @@
 
 package ciclos2;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-
-public class Ciclos2 {
-
+public class CiclosClassJOpctionPane {
     public static void main(String[] args) {
-        // Ejercicio con scanner
         
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite un Numero: ");
-        var numero = Integer.parseInt(entrada.nextLine());
+        var numero = Integer.parseInt(JOptionPane.showInputDialog("Digite Un Numero: "));
+        
         
         while (numero != 0) {
             if (numero > 0) {
@@ -24,12 +20,8 @@ public class Ciclos2 {
                 System.out.println("El numero " + numero+ " es NEGATIVO");
             }
             System.out.println("Digite otro numro: ");
-            numero = Integer.parseInt(entrada.nextLine());  
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro Numero: "));
         }
         System.out.println("EL NUMERO "+numero+" FINALIA EL PROGRAMA");
     }
-    
 }
-
-
-  
