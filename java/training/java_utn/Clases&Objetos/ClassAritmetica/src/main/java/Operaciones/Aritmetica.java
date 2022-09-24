aec2a6d331f871d1d3b60aa247d3307a106e7c71
federico pfund <federicopfund@gmail.com>
2022-09-23 21:35:29 -0300

    creamos un objetos y invocamos metodos desde main

---
+++ java/training/java_utn/Clases&Objetos/ClassAritmetica/src/main/java/Operaciones/Aritmetica.java
@@ -0,0 +1,16 @@
+
+package Operaciones;
+
+
+public class Aritmetica {
+    
+    // Atributos de la clase
+    int a;
+    int b;
+    
+    // metodo
+   public void sumarNumeros(){
+        int resultado = a +b ;
+         System.out.println("resultado = " + resultado);
+    } 
+}
