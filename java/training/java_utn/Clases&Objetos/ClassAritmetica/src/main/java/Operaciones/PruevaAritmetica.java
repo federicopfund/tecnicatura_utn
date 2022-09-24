3b8c25c0aaa97879220843ee0f27423318361798
federico pfund <federicopfund@gmail.com>
2022-09-23 21:33:23 -0300

    practica con objetos,invocamos metodos

---
+++ java/training/java_utn/Clases&Objetos/ClassAritmetica/src/main/java/Operaciones/PruevaAritmetica.java
@@ -0,0 +1,19 @@
+
+package Operaciones;
+
+
+public class PruevaAritmetica {
+    
+    public static void main(String[] args) {
+        // Nuevo Objeto llamando a un constructor
+        Aritmetica aritmetica1 = new Aritmetica();
+        // ingresamos valores al objeto a traves de invocar el 
+        // obj por su correspondiente atributo
+        aritmetica1.a = 3;
+        aritmetica1.b = 7;
+        // invocamos al metodo sumar numero:
+        aritmetica1.sumarNumeros();
+        
+        
+    }    
+}
