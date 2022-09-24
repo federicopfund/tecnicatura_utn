44243d816077715618584b595b7fa7a7a3959521
federico pfund <federicopfund@gmail.com>
2022-09-24 00:16:26 -0300

    practica con objetos,invocamos metodos

--- java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/Aritmetica.java
+++ java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/Aritmetica.java
@@ -11,7 +11,25 @@ public class Aritmetica {
     
     // metodo
    public void sumarNumeros(){
-        int resultado = a +b ;
+        int resultado = a + b ;
          System.out.println("resultado = " + resultado);
     } 
+
+      // tipo de retorno type: entero
+      public int sumarConRetorno(){
+        // retornara una exprecion
+        return a + b;
+    }
+    // tipo de retorno type: entero
+     public int sumarConArgumentos(int arg1,int arg2){
+         a = arg1;
+         b = arg2;
+         // operador this una ves que sale de la clase se destruye la variable.
+         // 
+         //this.a=arg1;
+         //this.b=arg2;
+         //return this.sumarConArgumentos(3,4);
+         return a + b;    
+     }
+ 
 }
