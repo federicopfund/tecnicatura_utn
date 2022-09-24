51579a394eb79ae43e6147271cabe476e99a1226
federico pfund <federicopfund@gmail.com>
2022-09-24 00:14:45 -0300

    practica con objetos,invocamos metodos

--- java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/PruevaAritmetica.java
+++ java/training/java_utn/Clases&Objetos/Aritmetica/src/main/java/Operaciones/PruevaAritmetica.java
@@ -1,9 +1,14 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Operaciones;
 
-
 public class PruevaAritmetica {
+   
     public static void main(String[] args) {
+        int resultado;
         // Nuevo Objeto llamando a un constructor
         Aritmetica aritmetica1 = new Aritmetica();
         // ingresamos valores al objeto a traves de invocar el 
@@ -12,7 +17,14 @@ public class PruevaAritmetica {
         aritmetica1.b = 7;
         // invocamos al metodo sumar numero:
         aritmetica1.sumarNumeros();
+            // crear una variable, type:int 
+        // invocar el metodo sumarConRetorno
+        int resutado = aritmetica1.sumarConRetorno();
+        System.out.println("resutado = " + resutado);
         
         
+        resultado = aritmetica1.sumarConArgumentos(2, 3);
+        System.out.println("resultado usando arg: " + resultado);
     }    
 }
+        
