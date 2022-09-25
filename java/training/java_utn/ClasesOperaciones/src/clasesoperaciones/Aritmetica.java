bfe13ad9b4c737adfd059053a6618af878f0305d
federico pfund <federicopfund@gmail.com>
2022-09-24 23:01:26 -0300

    ciclos y operaciones con clases e objetos

---
+++ java/training/java_utn/ClasesOperaciones/src/clasesoperaciones/Aritmetica.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasesoperaciones;
+
+/**
+ *
+ * @author feder
+ */
+public class Aritmetica {
+    // Atributos
+    int a;
+    int b;
+    
+    // Metodos
+    
+    public int sumarNumeros(){
+        return a + b;
+    }
+}
