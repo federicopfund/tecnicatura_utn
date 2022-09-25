e44f6551105389aab201acb49656d0acec90b419
federico pfund <federicopfund@gmail.com>
2022-09-25 02:17:13 -0300

    trabajamos en clase clases Scanner y JOptionPane

---
+++ java/training/java_utn/Ciclos/Ciclos2/src/ciclos2/cicloClaseScanner08.java
@@ -0,0 +1,17 @@
+
+package ciclos2;
+
+import javax.swing.JOptionPane;
+
+
+public class cicloClaseScanner08 {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        int i=1;
+        while(i<=numero){
+        JOptionPane.showMessageDialog(null,1);
+        i++;
+        }
+    }
+    
+}
