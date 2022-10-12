ccd12ee1fbfc08715eed79e5bfa9da40de5eed60
federico pfund <federicopfund@gmail.com>
2022-10-11 21:12:27 -0300

    challenge flow Control, methods, condicionales

--- java/training/java_utn/Condicionales/Condicionales8/src/condicionales8/Condicionales8.java
+++ java/training/java_utn/Condicionales/Condicionales8/src/condicionales8/Condicionales8.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package condicionales8;
 
 /**
@@ -11,29 +6,35 @@ package condicionales8;
  */
 
 import java.util.Scanner;
+
 public class Condicionales8 {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite un número entre 0 y 10: ");
-        var calificacion = Integer.parseInt(entrada.nextLine());
-        if (calificacion >= 9 && calificacion <= 10) {
-            System.out.println("A");            
-        }
-        else if (calificacion >= 8 && calificacion < 9){
-            System.out.println("B");
-        }
-        else if (calificacion >= 7 && calificacion < 8) {
-            System.out.println("C");
-        }
-        else if (calificacion >= 6 && calificacion < 7) {
-            System.out.println("D");
-        }
-        else if (calificacion >= 0 && calificacion < 6) {
-            System.out.println("F");
-        }
-        else{
-            System.out.println("Fuera de rango");
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Digite un número entre 0 y 10: ");
+            var calificacion = Integer.parseInt(entrada.nextLine());
+            if (calificacion >= 9 && calificacion <= 10) {
+                System.out.println("A");            
+            }
+            else if (calificacion >= 8 && calificacion < 9){
+                System.out.println("B");
+            }
+            else if (calificacion >= 7 && calificacion < 8) {
+                System.out.println("C");
+            }
+            else if (calificacion >= 6 && calificacion < 7) {
+                System.out.println("D");
+            }
+            else if (calificacion >= 0 && calificacion < 6) {
+                System.out.println("F");
+            }
+            else{
+                System.out.println("Fuera de rango");
+            }
+        } catch (NumberFormatException e) {
+            
+            e.printStackTrace();
         }
     }
     
