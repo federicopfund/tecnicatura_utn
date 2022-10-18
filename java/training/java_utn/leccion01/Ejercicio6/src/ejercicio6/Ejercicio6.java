ac59ad19e9b0eebf24b1c1d1f5c87fafcec33ca0
federico pfund <federicopfund@gmail.com>
2022-10-18 02:51:06 -0300

    { Clase : `Scanner` }{ Metodos : `println` `nextLine` `parseFloat` }

--- java/training/java_utn/leccion01/Ejercicio6/src/ejercicio6/Ejercicio6.java
+++ java/training/java_utn/leccion01/Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -18,6 +18,6 @@ public class Ejercicio6 {
         carlos = (matias + federico)/ 2;
         total = matias + carlos + emir;
         System.out.println("\nEl dinero de matias es: US$"+ mastias);
-        System.out.println("El dinero de carlos es: US$"+ carlos*100 + " + una Lamborghini Veneno y 2 Mellis");
+        System.out.println("El dinero de carlos es: US$"+ carlos*30);
         System.out.println("El total de dinero entre los tres es: US$"+ total);
     } 
