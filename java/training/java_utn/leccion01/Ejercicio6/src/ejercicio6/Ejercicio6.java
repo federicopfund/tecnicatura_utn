2c007d47e5cabd875be4476c5289f916b79d492f
federico pfund <federicopfund@gmail.com>
2022-10-18 01:53:42 -0300

    si me ayuda a recuperar a lilita

--- java/training/java_utn/leccion01/Ejercicio6/src/ejercicio6/Ejercicio6.java
+++ java/training/java_utn/leccion01/Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -10,10 +10,14 @@ public class Ejercicio6 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Introduzca la cantidad de dólares que tiene federico: ");
-        Double dolares = Double.parseDouble(entrada.nextLine());
-        var result = dolares * 2.25;
-        System.out.println("El total de dolares que tienen los tres es: U$D " +result);
-    }
-    
-}
+        float matias,carlos,federico,total;
+        System.out.println("Digite la cantidad de dinero de Guillermo: ");
+        federico = Float.parseFloat(entrada.nextLine());
+        
+        matias = federico / 2;
+        carlos = (matias + federico)/ 2;
+        total = matias + carlos + emir;
+        System.out.println("\nEl dinero de matias es: US$"+ mastias);
+        System.out.println("El dinero de carlos es: US$"+ carlos*100 + " + una Lamborghini Veneno y 2 Mellis");
+        System.out.println("El total de dinero entre los tres es: US$"+ total);
+    } 
