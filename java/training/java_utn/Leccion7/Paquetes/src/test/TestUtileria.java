7e61c7bbadc45a254d0197a1c59ede29a45c4100
federico pfund <federicopfund@gmail.com>
2022-11-12 18:19:34 -0300

    Test de los pakage de codersystem solo para metodos estaticos

--- java/training/java_utn/Leccion7/Paquetes/src/test/TestUtileria.java
+++ java/training/java_utn/Leccion7/Paquetes/src/test/TestUtileria.java
@@ -4,7 +4,8 @@ package test;
 //import ar.com.codesystem.*;
 import ar.com.codesystem.Utileria;
 //import static ar.com.codesystem.Utileria.imprimir; //solo aplica para métodos estáticos
-
+ 
+ 
 public class TestUtileria {
     public static void main(String[] args) {
         Utileria.imprimir("Saludos a todos los alumnos de la tecnicatura");
