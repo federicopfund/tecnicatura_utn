9f66cc34d1b400aeee461600ea5d1faca7343a86
federico pfund <federicopfund@gmail.com>
2022-11-26 23:57:07 -0300

    Controladora de Servicio

---
+++ java/training/SprintBoot/SprintFramework/src/main/java/Servicios/SpringConfigurador.java
@@ -0,0 +1,11 @@
+package Servicios;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan("Servicios")
+public class SpringConfigurador {
+
+}
