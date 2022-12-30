afd1af209134244e8edc2c79affbb0deb01f7f70
federico pfund <federicopfund@gmail.com>
2022-12-30 06:39:42 -0300

    Tipos de datos, operadores logicos, condicionales,

--- Python/leccion1/resource/ejercicio2/concatenaString.py
+++
@@ -1,7 +0,0 @@
-def concatenaString():
-    Empresa = "GroupbyCompany"
-    características = "enterprice"
-    print(f"Mi grupo favorito es: {Empresa}. características")
-# #Acá un ejemplo de como se puede usar el +
-    miGrupoFavorito2 = "Snarky Puppy"+". "+"The best contemporary Jazz Band"
-    print("Mi grupo favorito es: "+miGrupoFavorito2)
