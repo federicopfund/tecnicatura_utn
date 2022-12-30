ec39e7f20ae6de4f4bb672ecee8872d7012b9e2c
federico pfund <federicopfund@gmail.com>
2022-12-30 06:44:37 -0300

    refactoriacion

--- Python/leccion4/resource/calificacion.py
+++
@@ -1,8 +0,0 @@
-def calificacionpromedioymasbaja(calificaciones):
-    suma = 0
-    masbaja=0
-    for calificacion in calificaciones:
-        suma += calificacion
-        promedio = suma / len(calificaciones)
-        masbaja = min(calificaciones)
-    print(f" la suma promedio de calificaciones es: {promedio},Mientras que la nota mas baja es:{masbaja}")
--- Python/leccion4/resource/sum.py
+++
@@ -1,6 +0,0 @@
-def sumanumeros(N):
-    suma = 0
-    for i in range(1, N):
-        suma += i
-    print(f"valor de suma: {suma}")
-
