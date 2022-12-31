c36c938c5a64e41552525e003a9e927ebb53ff61
federico pfund <federicopfund@gmail.com>
2022-12-31 01:21:18 -0300

    Tienda de Libros, simula una mini biblioteca.

---
+++ java/training/java_utn/Algoritmos2/src/main/java/leccion1/TiendaLibros.java
@@ -0,0 +1,39 @@
+package leccion1;
+
+import java.util.Scanner;
+
+public class TiendaLibros {
+
+	private Scanner entrada;
+	private static TiendaLibros libro;
+	public TiendaLibros() {
+		entrada = new Scanner(System.in);
+        System.out.println("Digite en nombre del libro: ");
+        String nombreLibro = entrada.nextLine();
+        System.out.println("Digite el id del libro: ");
+        int idLibro = Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite el precio del libro: ");
+        double precioLibro = Double.parseDouble(entrada.nextLine());
+        System.out.println("Confirme si el envio es gratuito: ");
+        boolean envioGratuito = Boolean.parseBoolean(entrada.nextLine());
+        
+        System.out.println(nombreLibro+" #"+idLibro);
+        System.out.println("Precio del libro: $"+ precioLibro);
+        System.out.println("El envio del libro gratuito es: "+envioGratuito);
+    
+	}
+
+	public static void main(String[] args) {
+		setLibro(new TiendaLibros());
+
+	}
+
+	public static TiendaLibros getLibro() {
+		return libro;
+	}
+
+	public static void setLibro(TiendaLibros libro) {
+		TiendaLibros.libro = libro;
+	}
+
+}
