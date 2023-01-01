7e31fbd23e67396219689bb23cc71d8679a894a4
federico pfund <federicopfund@gmail.com>
2022-12-31 23:11:33 -0300

    Test de prueva

---
+++ java/training/java_utn/Algoritmos2/src/main/java/leccion7/Paquetes/Test/TestUtileria.java
@@ -0,0 +1,13 @@
+package leccion7.Paquetes.Test;
+
+import leccion7.Paquetes.Dominio.Utileria;
+
+public class TestUtileria {
+
+	public static void main(String[] args) {
+		  Utileria.imprimir("Saludos a todos los alumnos de la tecnicatura");
+	        
+	    }
+
+	
+	}
