dc70090b3e9f4a6e4ccadffc0ee3523c74e43af3
federico pfund <federicopfund@gmail.com>
2022-12-31 23:46:55 -0300

    Tets de clase Persona  arreglos de Edades.

--- java/training/java_utn/Algoritmos2/src/main/java/leccion10/Matrices/test/TestArreglo.java
+++ java/training/java_utn/Algoritmos2/src/main/java/leccion10/Matrices/test/TestArreglo.java
@@ -3,8 +3,22 @@ package leccion10.Matrices.test;
 public class TestArreglo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 int  edades[]  = new int [3]; //el lado izq. declaramos la variable
+	        System.out.println("edades = " + edades);
+	        
+	        edades[0] = 17;
+	        System.out.println("edades 0 = " + edades[0]);
+	        
+	        edades[1] = 22;
+	        System.out.println("edades 1 = " + edades[1]);
+	        
+	        edades[2] = 18;
+	        System.out.println("edades 2 = " + edades[2]);
+	        
+	        //edades[3] = 7; // Fuera de rango, error en tiempo de ejecución
+	        
+	        for(int i = 0; i < edades.length; i++){
+	            System.out.println("edades y sus elementos "+i+": "+edades[i]);
+	        }
+	    }
 	}
-
-}
