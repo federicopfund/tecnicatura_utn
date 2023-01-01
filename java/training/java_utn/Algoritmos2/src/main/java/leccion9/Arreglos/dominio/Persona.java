88206e392afefbb132beda9d8364c91ba31329ec
federico pfund <federicopfund@gmail.com>
2022-12-31 23:30:40 -0300

    Clase Base  Persona, metodos `Getter` & `Setter` .

---
+++ java/training/java_utn/Algoritmos2/src/main/java/leccion9/Arreglos/dominio/Persona.java
@@ -0,0 +1,14 @@
+package leccion9.Arreglos.dominio;
+
+public class Persona {
+
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
