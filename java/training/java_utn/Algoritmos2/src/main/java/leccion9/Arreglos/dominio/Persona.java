6ff39fca8908743f6aa6f0db479a68302681f4b8
federico pfund <federicopfund@gmail.com>
2022-12-31 23:43:54 -0300

    Clase Base Persona, con sus metodos setter & getter.

--- java/training/java_utn/Algoritmos2/src/main/java/leccion9/Arreglos/dominio/Persona.java
+++ java/training/java_utn/Algoritmos2/src/main/java/leccion9/Arreglos/dominio/Persona.java
@@ -1,14 +1,23 @@
 package leccion9.Arreglos.dominio;
 
+
 public class Persona {
+    private String nombre;
 
-	public Persona() {
-		// TODO Auto-generated constructor stub
-	}
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public String getNombre() {
+        return nombre;
+    }
 
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + '}'+", "+super.toString();
+    }
 }
