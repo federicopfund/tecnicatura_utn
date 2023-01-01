88be35f42a9f4a5f4f873b93b327aa065d44e9c7
federico pfund <federicopfund@gmail.com>
2022-12-31 23:21:57 -0300

    Clase Empleado Extiende de Persona. `Override`

---
+++ java/training/java_utn/Algoritmos2/src/main/java/leccion8/PalabraFinal/Dominio/Empleado.java
@@ -0,0 +1,8 @@
+package leccion8.PalabraFinal.Dominio;
+
+public class Empleado extends Persona{
+    @Override
+    public void imprimir(){
+        System.out.println("Método imprimir desde la clase hija");
+    }
+}
