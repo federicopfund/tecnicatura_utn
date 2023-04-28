9397ead294e8a7739a6c6773dde6b24a79b1ec45
federico pfund <federicopfund@gmail.com>
2023-04-27 23:21:21 -0300

    Test de Enumeraciones de dias.

---
+++ java/training/java_utn/Algoritmo3/src/leccion02/ForEch/enumeraciones/Test/TestEnumeraciones.java
@@ -0,0 +1,9 @@
+package leccion02.ForEch.enumeraciones.Test;
+
+import leccion02.ForEch.enumeraciones.Dias;
+
+public class TestEnumeraciones {
+	public static void main(String[] args) {
+		System.out.println("Dias: "+Dias.LUNES);
+	}
+}
