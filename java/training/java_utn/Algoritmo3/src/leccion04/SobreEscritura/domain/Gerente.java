e48fbc60e4e5b818b48d00ddee3094db21078a09
federico pfund <federicopfund@gmail.com>
2023-05-04 01:54:27 -0300

    Clase Gerente metodo Sobreescritura

---
+++ java/training/java_utn/Algoritmo3/src/leccion04/SobreEscritura/domain/Gerente.java
@@ -0,0 +1,15 @@
+package leccion04.SobreEscritura.domain;
+
+public class Gerente extends Empleado {
+	private String departamento;
+
+	public Gerente(String nombre,double sueldo,String departamento) {
+		super(nombre,sueldo);
+		this.departamento = departamento;
+	}
+	// Sobreescribimos el metodo
+	@Override
+	public String obtenerDetalles() {
+		return super.obtenerDetalles()+", Departamento: "+this.departamento;
+	}
+}
