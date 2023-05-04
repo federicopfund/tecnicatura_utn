8689e7d614473c10fed19e9dd43fdbbe9bd9cf75
federico pfund <federicopfund@gmail.com>
2023-05-04 18:07:18 -0300

    Clase Empleado  Polimorfismo.

---
+++ java/training/java_utn/Algoritmo3/src/leccion04/InstanciaOf/domain/Empleado.java
@@ -0,0 +1,33 @@
+package leccion04.InstanciaOf.domain;
+
+public class Empleado {
+
+	protected String nombre;
+	protected double sueldo;
+	
+	public Empleado(String nombre,double sueldo) {
+		this.nombre= nombre;
+		this.sueldo = sueldo;
+	}
+	
+	// Metodos para la Sobreescritura
+	public String obtenerDetalles() {
+		return "Nombre :"+this.nombre+" Sueldo: "+this.sueldo;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public double getSueldo() {
+		return sueldo;
+	}
+	
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
+}
