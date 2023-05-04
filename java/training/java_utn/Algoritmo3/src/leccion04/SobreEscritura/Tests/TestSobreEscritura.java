7530a6d9509fe0a6a679706b04f65415eaff7af5
federico pfund <federicopfund@gmail.com>
2023-05-04 18:08:36 -0300

    Clase Test de Sobreescritura

--- java/training/java_utn/Algoritmo3/src/leccion04/SobreEscritura/Tests/TestSobreEscritura.java
+++ java/training/java_utn/Algoritmo3/src/leccion04/SobreEscritura/Tests/TestSobreEscritura.java
@@ -1,12 +1,20 @@
 package leccion04.SobreEscritura.Tests;
 
+import leccion04.SobreEscritura.domain.Empleado;
 import leccion04.SobreEscritura.domain.Gerente;
 
 public class TestSobreEscritura {
 
 	public static void main(String[] args) {
-		Gerente gerente = new Gerente("Jose",5000,"Sistema");
+		Empleado empleado = new Empleado("Juan", 1000);
+		Gerente gerente = new Gerente("Jose", 5000, "Sistema");
+		imprimir(gerente);
 		System.out.println("Gerente: "+ gerente.obtenerDetalles());
 	}
-
+	
+	public static void imprimir(Empleado empleado) {
+		String detalle = empleado.obtenerDetalles();
+		System.out.println("detalles = " + detalle);
+		System.out.println("empleado = "+ empleado.obtenerDetalles());
+	}
 }
